499804937a18967ddce7301b3f800613a72289eb
Atibhi Agrawal <Atibhi.Agrawal@iiitb.org>
2018-12-17 00:02:19 +0530

    Delete all files

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions' }
-  authenticated :user do
-    root to: 'home#signedin', as: :authenticated_root
-  end
-  get 'home/index'
-  root 'home#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
