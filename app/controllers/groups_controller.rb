c8efaf021ce65c41c658c21e928681dc25c26ef6
Atibhi Agrawal <Atibhi.Agrawal@iiitb.org>
2018-12-24 11:13:49 +0530

    Commit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,6 @@
 class GroupsController < ApplicationController
     before_action :authenticate_user!
-    #find institute name of user
-    #find group==institute name
-    #add to group
-    #display groups
-    
+
     def index
         user_institute = current_user.institute
         @group = Group.where(name: user_institute).take
@@ -12,8 +8,7 @@ class GroupsController < ApplicationController
             puts()
         else
             current_user.groups << @group
-        end
-        
+        end        
     end
 
    
