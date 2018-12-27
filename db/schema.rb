50cbb440903b5df541d5ef7a94621509c68babc8
Atibhi Agrawal <Atibhi.Agrawal@iiitb.org>
2018-12-27 23:01:34 +0530

    admin dashboard, admin attribute added to user, kaminari configured

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181227114502) do
+ActiveRecord::Schema.define(version: 20181227172217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,8 +43,8 @@ ActiveRecord::Schema.define(version: 20181227114502) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "deadline"
-    t.string "image"
     t.string "rel_link"
+    t.string "image"
     t.string "venue"
     t.index ["category_id"], name: "index_posts_on_category_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20181227114502) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
