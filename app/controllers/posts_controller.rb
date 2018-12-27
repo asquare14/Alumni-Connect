c011653284bdf4b4fa9391cc36abcd84c1c51c18
Atibhi Agrawal <atibhi.a@gmail.com>
2018-12-27 17:32:27 +0530

    Venue Added for Events (#38)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -58,7 +58,7 @@ class PostsController < ApplicationController
     end
     
     def post_params
-        params.require(:post).permit(:content, :title, :category_id, :deadline, :rel_link,:image).merge(user_id: current_user.id)
+        params.require(:post).permit(:content, :title, :category_id, :deadline, :rel_link,:image,:venue).merge(user_id: current_user.id)
     end
     
     def get_posts
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181227110806) do
+ActiveRecord::Schema.define(version: 20181227114502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20181227110806) do
     t.date "deadline"
     t.string "rel_link"
     t.string "image"
+    t.string "venue"
     t.index ["category_id"], name: "index_posts_on_category_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
