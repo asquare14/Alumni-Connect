3144b0d1a52fdf37a5ba46e216f2262a6c92594a
Atibhi Agrawal <Atibhi.Agrawal@iiitb.org>
2018-12-28 18:25:53 +0530

    UI of Jobs, Upvote Button fixed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,6 @@ class PostsController < ApplicationController
     
     def show
         @post = Post.find(params[:id])
-        
     end
     
     def hobby
@@ -37,7 +36,7 @@ class PostsController < ApplicationController
     def upvote
         @post = Post.find(params[:id])
         @post.upvote_by current_user
-        redirect_to post_path(@post) 
+        redirect_to study_posts_path 
     end
        
     def downvote
