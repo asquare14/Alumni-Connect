8949c404e8bf1e62dcda36678cf8706f29e1fd62
Atibhi Agrawal <Atibhi.Agrawal@iiitb.org>
2018-12-30 00:59:25 +0530

    study like, read more

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  
   devise_for :users, :controllers => {:registrations => "registrations"}
   
   devise_scope :user do
