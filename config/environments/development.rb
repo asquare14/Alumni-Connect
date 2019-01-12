21e4273ff70646e6616affd65da0cae7f3bca283
Atibhi Agrawal <Atibhi.Agrawal@iiitb.org>
2019-01-12 23:11:48 +0530

    messaging feature

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Verifies that versions and hashed value of the package contents in the project's package.json
-config.webpacker.check_yarn_integrity = true
+  config.webpacker.check_yarn_integrity = true
+  config.action_mailer.default_url_options = { :host => "dev.yourhost.com" }
 
   # Settings specified here will take precedence over those in config/application.rb.
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Verifies that versions and hashed value of the package contents in the project's package.json
-config.webpacker.check_yarn_integrity = false
+  config.webpacker.check_yarn_integrity = false
+  config.action_mailer.default_url_options = { :host => "dev.yourhost.com" }
+
 
   # Settings specified here will take precedence over those in config/application.rb.
 
